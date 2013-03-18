073c2d12c6035fd6c7f7da2c6828648b28f016bf
oasisfeng <git@id.oasisfeng.com>
2013-03-19 01:30:25 +0800

    Re-launch activity after translation entry in AboutFragment is clicked.

--- src/com/oasisfeng/ui/AboutActivity.java
+++ src/com/oasisfeng/ui/AboutActivity.java
@@ -67,6 +67,8 @@ public class AboutActivity extends PreferenceActivity {
                 final Locale default_locale = Locale.getDefault();
                 final Locale locale = Locales.getFrom(getActivity());
                 Locales.switchTo(getActivity(), locale == null || default_locale.equals(locale) ? new Locale("en") : default_locale);
+                getActivity().recreate();
+                return true;
             }
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
