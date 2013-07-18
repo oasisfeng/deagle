342976889b165a01a999e19cbf1fbe3ffe7e47f2
oasisfeng <git@id.oasisfeng.com>
2013-07-18 23:00:19 +0800

    REFACTOR: Minor tweaks.

--- src/com/oasisfeng/android/base/Format.java
+++ src/com/oasisfeng/android/base/Format.java
@@ -7,22 +7,18 @@ import android.text.format.DateUtils;
 public class Format {
 
     @SuppressLint("DefaultLocale")    // TODO: Use locale format
-    public static String elapsedTime(final StringBuilder recycle, long elapsedSeconds) {
+    public static String elapsedTime(final StringBuilder recycle, final long elapsedSeconds) {
         if (elapsedSeconds < 100 * 60 * 60) return DateUtils.formatElapsedTime(recycle, elapsedSeconds);
 
-        long hours = 0;
-        long minutes = 0;
-        long seconds = 0;
-
-        if (elapsedSeconds >= 3600) {
-            hours = elapsedSeconds / 3600;
-            elapsedSeconds -= hours * 3600;
+        long seconds = elapsedSeconds, hours = 0, minutes = 0;
+        if (seconds >= 3600) {
+            hours = seconds / 3600;
+            seconds = hours % 3600;
         }
-        if (elapsedSeconds >= 60) {
-            minutes = elapsedSeconds / 60;
-            elapsedSeconds -= minutes * 60;
+        if (seconds >= 60) {
+            minutes = seconds / 60;
+            seconds = minutes % 60;
         }
-        seconds = elapsedSeconds;
         return String.format("%1$d:%2$02d:%3$02d", hours, minutes, seconds);
     }
 }
