ca6db820006aefdeb8f8b5c31375114f9466793a
oasisfeng <git@id.oasisfeng.com>
2013-09-23 23:49:50 +0800

    UPDATE: Reworked the intent update logic within class GooglePlayStore. ADD: GooglePlayStore.isAvailable().

--- src/com/oasisfeng/android/google/GooglePlayStore.java
+++ src/com/oasisfeng/android/google/GooglePlayStore.java
@@ -3,8 +3,7 @@ package com.oasisfeng.android.google;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.preference.Preference;
@@ -12,14 +11,22 @@ import android.preference.Preference;
 /** @author Oasis */
 public class GooglePlayStore {
 
-    private static final String PLAY_SCHEME = "market";
     private static final String PLAY_PACKAGE_NAME = "com.android.vending";
     private static final String PLAY_URL_PREFIX = "https://play.google.com/store/apps/details?id=";
-    private static final String PLAY_HOST = "play.google.com";
+
+    public static boolean isAvailable(final Context context) {
+        try {
+            final ApplicationInfo play_app = context.getPackageManager().getApplicationInfo(PLAY_PACKAGE_NAME, 0);
+            return play_app.enabled;
+        } catch (final NameNotFoundException e) {
+            return false;
+        }
+
+    }
 
     public static void showApp(final Context context, final String pkg) {
         final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(PLAY_URL_PREFIX + pkg));
-        updatePlayUrlIntent(context.getPackageManager(), intent);
+        updatePlayUrlIntent(context, intent);
         try { context.startActivity(intent);
         } catch(final ActivityNotFoundException e) { /* In case of Google Play malfunction */ }
     }
@@ -31,16 +38,16 @@ public class GooglePlayStore {
 
     public static void updatePreferenceIntent(final Context context, final Preference preference) {
         final Intent intent = preference.getIntent();
-        if (intent == null) return;
-        final Uri uri = intent.getData();
-        if (uri == null || PLAY_SCHEME.equals(uri.getScheme()) || ! PLAY_HOST.equals(uri.getHost())) return;
-        updatePlayUrlIntent(context.getPackageManager(), intent);
+        updatePlayUrlIntent(context, intent);
     }
 
-    private static void updatePlayUrlIntent(final PackageManager pm, final Intent intent) {
-        try {
-            final PackageInfo play_pkg = pm.getPackageInfo(PLAY_PACKAGE_NAME, 0);
-            if (play_pkg.applicationInfo.enabled) intent.setPackage(PLAY_PACKAGE_NAME);
-        } catch (final NameNotFoundException e) { /* Fall-through */ }
+    /** Modify intent to launch Google Play Store directly if possible (without opener-app selector) */
+    private static void updatePlayUrlIntent(final Context context, final Intent intent) {
+        if (intent == null || intent.getPackage() != null) return;      // Skip intent with explicit target package
+        final Uri uri = intent.getData();
+        if (uri == null) return;
+        intent.setPackage(PLAY_PACKAGE_NAME);
+        if (intent.resolveActivity(context.getPackageManager()) != null) return;
+        intent.setPackage(null);
     }
 }
