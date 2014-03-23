f0504fee6fb4031fcd361c3520a3eeb44d85fe67
Oasis <Oasis@Oasis-PC>
2014-03-23 17:15:02 +0800

    ADD: Apps.getAppName() and Apps.getAppNames().

--- src/com/oasisfeng/android/util/Apps.java
+++ src/com/oasisfeng/android/util/Apps.java
@@ -1,9 +1,11 @@
 package com.oasisfeng.android.util;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 
 /** @author Oasis */
@@ -32,6 +34,42 @@ public class Apps {
         } catch(final IllegalArgumentException e) {
             return false;       // Should never happen
         }
+    }
+
+    public CharSequence getAppName(final String pkg) {
+        final PackageManager pm = mContext.getPackageManager();
+        ApplicationInfo info;
+        try {
+        	info = pm.getApplicationInfo(pkg, 0);
+        } catch (final NameNotFoundException e) {
+        	return "<?>";
+        }
+        try {
+        	return info.loadLabel(pm);
+        } catch (final RuntimeException e) {	// Including Resources.NotFoundException
+        	return info.packageName;
+        }
+    }
+
+    public String getAppNames(final Collection<String> pkgs, final String separator) {
+        final StringBuilder app_names = new StringBuilder();
+        final PackageManager pm = mContext.getPackageManager();
+        for (final String pkg : pkgs) {
+        	app_names.append(separator);
+            ApplicationInfo info;
+            try {
+            	info = pm.getApplicationInfo(pkg, 0);
+            } catch (final NameNotFoundException e) {
+            	app_names.append("<?>");
+            	continue;
+            }
+            try {
+            	app_names.append(info.loadLabel(pm));
+            } catch (final RuntimeException e) {	// Including Resources.NotFoundException
+            	app_names.append(info.packageName);
+            }
+        }
+        return app_names.substring(separator.length());
     }
 
     private Apps(final Context context) {
