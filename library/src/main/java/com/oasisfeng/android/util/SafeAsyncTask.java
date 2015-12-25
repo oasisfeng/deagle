3f35555c0a7d5fce98abef453a4f5980a7644b68
Oasis <git@id.oasisfeng.com>
2015-12-26 00:51:52 +0800

    Improved SafeAsyncTask with various static execute() methods.

--- library/src/main/java/com/oasisfeng/android/util/SafeAsyncTask.java
+++ library/src/main/java/com/oasisfeng/android/util/SafeAsyncTask.java
@@ -1,8 +1,10 @@
 package com.oasisfeng.android.util;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.AsyncTask;
-import android.os.Build;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
 import android.util.Log;
 
 import java.lang.ref.WeakReference;
@@ -11,18 +13,68 @@ import java.lang.reflect.Method;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 
-/** @author Oasis */
+/**
+ * Protect against NPE when calling {@link Fragment#getActivity()} in {@link AsyncTask}.
+ *
+ * Note: Avoiding memory leak is not the purpose of this class. Please consider using lambda (with RetroLambda)
+ *
+ * @author Oasis
+ */
 public abstract class SafeAsyncTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
 
-    /** This method should be called in application initialization, to make sure
-	 *  AsyncTask's internal handler is created on the main thread. */
-	public static void init() { /* Do nothing besides static initialization. */ }
+	public interface Consumer<T> {
+		void accept(T object);
+	}
+
+	public static void execute(final Activity activity, final Consumer<Activity> runnable) {
+		final WeakReference<Activity> reference = new WeakReference<>(activity);
+		AsyncTask.execute(new SafeTask<>(new Supplier<Activity>() { @Override public Activity get() {
+			return ifActive(reference.get());
+		}}, runnable));
+	}
+
+	public static void execute(final Fragment fragment, final Consumer<Activity> runnable) {
+		final WeakReference<Fragment> fragment_reference = new WeakReference<>(fragment);
+		AsyncTask.execute(new SafeTask<>(new Supplier<Activity>() { @Override public Activity get() {
+			final Fragment fragment = fragment_reference.get();
+			return fragment == null ? null : fragment.getActivity();
+		}}, runnable));
+	}
+
+	public static void execute(final android.support.v4.app.Fragment fragment, final Consumer<Activity> runnable) {
+		final WeakReference<android.support.v4.app.Fragment> fragment_reference = new WeakReference<>(fragment);
+		AsyncTask.execute(new SafeTask<>(new Supplier<Activity>() { @Override public Activity get() {
+			final android.support.v4.app.Fragment fragment = fragment_reference.get();
+			return fragment == null ? null : fragment.getActivity();
+		}}, runnable));
+	}
+
+	private interface Supplier<T> {
+		T get();
+	}
+
+	static class SafeTask<T> implements java.lang.Runnable {
+
+		SafeTask(final Supplier<T> supplier, final Consumer<T> runnable) {
+			mSupplier = supplier;
+			mRunnable = runnable;
+		}
+
+		@Override public void run() {
+			final T object = mSupplier.get();
+			if (object == null) return;
+			mRunnable.accept(object);
+		}
+
+		private final Supplier<T> mSupplier;
+		private final Consumer<T> mRunnable;
+	}
 
 	public SafeAsyncTask(final Activity activity) {
 		mActivityRef = new WeakReference<>(activity);
 	}
 
-    protected void onProgressUpdate(final Activity activity, final Progress... values) {}
+	@SafeVarargs protected final void onProgressUpdate(final Activity activity, final Progress... values) {}
     protected void onPostExecute(final Activity activity, final Result result) {}
     protected void onCancelled(final Activity activity, final Result result) {}
 
@@ -56,32 +108,27 @@ public abstract class SafeAsyncTask<Params, Progress, Result> extends AsyncTask<
     @Override protected final void onCancelled() {}
 
 	@SafeVarargs @Override protected final void onProgressUpdate(final Progress... values) {
-        final Activity activity = getActivityIfAvailable();
+        final Activity activity = ifActive(mActivityRef.get());
         if (activity == null) return;
         onProgressUpdate(activity, values);
 	}
 
     @Override protected final void onPostExecute(final Result result) {
-	    final Activity activity = getActivityIfAvailable();
+	    final Activity activity = ifActive(mActivityRef.get());
 	    if (activity == null) return;
 		onPostExecute(activity, result);
 	}
 
     @Override protected final void onCancelled(final Result result) {
-        final Activity activity = getActivityIfAvailable();
+        final Activity activity = ifActive(mActivityRef.get());
         if (activity == null) return;
         onCancelled(activity, result);
     }
 
     /** Check activity status to avoid crash due to referencing activity which is no longer available. */
-    private Activity getActivityIfAvailable() {
-        final Activity activity = mActivityRef.get();
-	    if (activity == null || activity.isFinishing()
-	            || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 && activity.isDestroyed())) {
-	        mActivityRef.clear();      // Avoid duplicate check
-	        return null;
-	    }
-        return activity;
+    private static Activity ifActive(final Activity activity) {
+	    return activity == null || activity.isFinishing()
+	            || (VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN_MR1 && activity.isDestroyed()) ? null : activity;
     }
 
 	private final WeakReference<Activity> mActivityRef;
