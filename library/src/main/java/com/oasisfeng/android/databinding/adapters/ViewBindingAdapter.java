dfc8d29018d8d3f9865828c65dcf5184e7c608ca
Oasis <git@id.oasisfeng.com>
2016-05-05 23:01:24 +0800

    UPDATE: Use ":visible" instead of "android:visible" to avoid confusion.

--- library/src/main/java/com/oasisfeng/android/databinding/adapters/ViewBindingAdapter.java
+++ library/src/main/java/com/oasisfeng/android/databinding/adapters/ViewBindingAdapter.java
@@ -4,14 +4,14 @@ import android.databinding.BindingAdapter;
 import android.view.View;
 
 /**
- * Binding adapter for {@link View}
+ * Complementary binding adapter for {@link View}
  *
  * Created by Oasis on 2016/4/25.
  */
 @SuppressWarnings("unused")
 public class ViewBindingAdapter {
 
-	@BindingAdapter("android:visible")
+	@BindingAdapter("visible")
 	public static void setVisibility(final View view, final boolean visible) {
 		view.setVisibility(visible ? View.VISIBLE : View.GONE);
 	}
