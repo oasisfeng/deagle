4ff87be9164db2946fbd14bc9c72f657c12adcc4
Oasis <git@id.oasisfeng.com>
2016-08-25 21:41:32 +0800

    ADD: TextViewBindingAdapter for binding string resource ID to TextView.

---
+++ library/src/main/java/com/oasisfeng/android/databinding/adapters/TextViewBindingAdapter.java
@@ -0,0 +1,19 @@
+package com.oasisfeng.android.databinding.adapters;
+
+import android.databinding.BindingAdapter;
+import android.support.annotation.StringRes;
+import android.widget.TextView;
+
+/**
+ * Extra bindings for {@link android.widget.TextView}
+ *
+ * Created by Oasis on 2016/8/23.
+ */
+@SuppressWarnings("unused")
+public class TextViewBindingAdapter {
+
+	@BindingAdapter("text")
+	public static void setText(final TextView view, final @StringRes int text_res) {
+		view.setText(text_res != 0 ? view.getContext().getText(text_res) : null);
+	}
+}
