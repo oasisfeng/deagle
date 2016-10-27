0d053283ee6a08a52062bed04ba2ebc4f595a4b6
Oasis <git@id.oasisfeng.com>
2016-10-28 02:04:35 +0800

    UPDATE: Add Apps.isPrivileged().

--- library/src/main/java/com/oasisfeng/android/util/Apps.java
+++ library/src/main/java/com/oasisfeng/android/util/Apps.java
@@ -6,9 +6,15 @@ import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Build;
 import android.support.annotation.CheckResult;
+import android.util.Log;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Collection;
+
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.KITKAT;
+import static android.os.Build.VERSION_CODES.M;
 
 /** @author Oasis */
 public class Apps {
@@ -29,9 +35,10 @@ public class Apps {
 
     /** Check whether specified app is installed in current user, even if hidden by system (Android 5+). */
     public @CheckResult boolean isInstalledInCurrentUser(final String pkg) {
-        try { //noinspection WrongConstant,deprecation
+        try { @SuppressWarnings("deprecation")
             final ApplicationInfo info = mContext.getPackageManager().getApplicationInfo(pkg, PackageManager.GET_UNINSTALLED_PACKAGES);
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) return true;
+            //noinspection SimplifiableIfStatement
+            if (SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) return true;
             return (info.flags & ApplicationInfo.FLAG_INSTALLED) != 0;
         } catch (final NameNotFoundException e) {
             return false;
@@ -41,6 +48,38 @@ public class Apps {
     /** Use {@link #isInstalledInCurrentUser(String)} or {@link #isInstalledOnDevice(String)} instead */
     @Deprecated public @CheckResult boolean isInstalled(final String pkg) {
         return isInstalledInCurrentUser(pkg);
+    }
+
+    public static boolean isPrivileged(final ApplicationInfo app) {
+        if (SDK_INT >= M) {
+            if (ApplicationInfo_privateFlags == NO_SUCH_FIELD) return isSystem(app);    // Fallback
+            try {
+                if (ApplicationInfo_privateFlags == null) {
+                    ApplicationInfo_privateFlags = ApplicationInfo.class.getField("privateFlags");
+                    if (ApplicationInfo_privateFlags.getType() != int.class) throw new NoSuchFieldException();
+                }
+                return ((int) ApplicationInfo_privateFlags.get(app) & PRIVATE_FLAG_PRIVILEGED) != 0;
+            } catch (final NoSuchFieldException | IllegalAccessException | MultiCatchROECompat e) {
+                ApplicationInfo_privateFlags = NO_SUCH_FIELD;
+                Log.e(TAG, "Incompatible ROM: No public integer field - ApplicationInfo.privateFlags");
+                return isSystem(app);
+            }
+        }
+        return SDK_INT >= KITKAT && (app.flags & FLAG_PRIVILEGED) != 0;
+    }
+    private static final int FLAG_PRIVILEGED = 1<<30;
+    private static final int PRIVATE_FLAG_PRIVILEGED = 1<<3;        // ApplicationInfo.PRIVATE_FLAG_PRIVILEGED
+
+    private static boolean isSystem(final ApplicationInfo app) {
+        return (app.flags & ApplicationInfo.FLAG_SYSTEM) != 0;
+    }
+
+    private static Field ApplicationInfo_privateFlags;
+    private static final Field NO_SUCH_FIELD;
+    static {
+        try {
+            NO_SUCH_FIELD = Apps.class.getDeclaredField("NO_SUCH_FIELD");
+        } catch (final NoSuchFieldException e) { throw new LinkageError(); }
     }
 
     public @CheckResult boolean isEnabled(final String pkg) throws NameNotFoundException {
@@ -108,4 +147,5 @@ public class Apps {
     }
 
     private final Context mContext;
+    private static final String TAG = "Apps";
 }
