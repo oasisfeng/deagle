8cd93330788f4b62fcd1f4ba9d418dba43f8f07e
Oasis <git@id.oasisfeng.com>
2016-11-10 18:04:26 +0800

    ADD: IoUtils.

---
+++ library/src/main/java/com/oasisfeng/java/utils/IoUtils.java
@@ -0,0 +1,28 @@
+package com.oasisfeng.java.utils;
+
+import android.support.annotation.Nullable;
+
+import java.io.Closeable;
+import java.io.InputStream;
+
+/** Simple static methods to perform common IO operations. */
+public final class IoUtils {
+
+	public static void closeQuietly(final @Nullable Closeable closeable) {
+		if (closeable == null) return;
+		try {
+			closeable.close();
+		} catch (final RuntimeException e) {
+			throw e;
+		} catch (final Exception ignored) {}
+	}
+
+	public static void closeQuietly(final @Nullable InputStream stream) {
+		if (stream == null) return;
+		try {
+			stream.close();
+		} catch (final RuntimeException e) {
+			throw e;
+		} catch (final Exception ignored) {}
+	}
+}
