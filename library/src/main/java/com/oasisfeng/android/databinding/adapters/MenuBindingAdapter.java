ad6c64772a760397619fd999cde6fc9f12201152
Oasis <git@id.oasisfeng.com>
2016-12-01 23:05:58 +0800

    ADD: MenuBindingAdapter, menu binding adapter for Toolbar and ActionMenuView.

---
+++ library/src/main/java/com/oasisfeng/android/databinding/adapters/MenuBindingAdapter.java
@@ -0,0 +1,38 @@
+package com.oasisfeng.android.databinding.adapters;
+
+import android.databinding.BindingAdapter;
+import android.databinding.BindingConversion;
+import android.support.annotation.MenuRes;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ActionMenuView;
+import android.widget.Toolbar;
+
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
+
+/**
+ * Binding adapter for menu in {@link Toolbar} and {@link ActionMenuView}
+ *
+ * Created by Oasis on 2016/11/30.
+ */
+public class MenuBindingAdapter {
+
+	@BindingConversion public static int convertMenu(final int menu_res) { return menu_res; }
+
+	@BindingAdapter("menu") public static void inflateMenu(final Toolbar toolbar, final @MenuRes int old_menu, final @MenuRes int new_menu) {
+		if (SDK_INT < LOLLIPOP) return;
+		if (new_menu == old_menu) return;
+		final Menu menu = toolbar.getMenu();
+		menu.clear();
+		toolbar.inflateMenu(new_menu);
+	}
+
+	@BindingAdapter("menu") public static void inflateMenu(final ActionMenuView amv, final @MenuRes int old_menu, final @MenuRes int new_menu) {
+		if (SDK_INT < LOLLIPOP) return;
+		if (new_menu == old_menu) return;
+		final Menu menu = amv.getMenu();
+		menu.clear();
+		new MenuInflater(amv.getContext()).inflate(new_menu, menu);
+	}
+}
