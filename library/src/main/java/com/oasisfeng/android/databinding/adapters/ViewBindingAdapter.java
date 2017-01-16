672841895b41c9ee4df2ddaff339658c2fa1d37a
Oasis <git@id.oasisfeng.com>
2017-01-16 10:51:54 +0800

    ADD: ViewBindingAdapter now supports layout margin attributes.

--- library/src/main/java/com/oasisfeng/android/databinding/adapters/ViewBindingAdapter.java
+++ library/src/main/java/com/oasisfeng/android/databinding/adapters/ViewBindingAdapter.java
@@ -1,7 +1,9 @@
 package com.oasisfeng.android.databinding.adapters;
 
 import android.databinding.BindingAdapter;
+import android.os.Build;
 import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Complementary binding adapter for {@link View}
@@ -20,4 +22,21 @@ public class ViewBindingAdapter {
 	public static void setShown(final View view, final boolean shown) {
 		view.setVisibility(shown ? View.VISIBLE : View.GONE);
 	}
+
+	@BindingAdapter(requireAll = false, value = {"android:layout_marginTop", "android:layout_marginBottom",
+			"android:layout_marginStart", "android:layout_marginEnd", "android:layout_marginLeft", "android:layout_marginRight"})
+	public static void setLayoutWidth(final View view, final Float top, final Float bottom, final Float start, final Float end, final Float left, final Float right) {
+		final ViewGroup.LayoutParams layout_params = view.getLayoutParams();
+		if (! (layout_params instanceof ViewGroup.MarginLayoutParams)) return;
+		final ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) layout_params;
+		//noinspection ResourceType
+		params.setMargins(v(left, params.leftMargin), v(top, params.topMargin), v(right, params.rightMargin), v(bottom, params.bottomMargin));
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+			if (start != null) params.setMarginStart(start.intValue());
+			if (end != null) params.setMarginEnd(end.intValue());
+		}
+		view.setLayoutParams(params);
+	}
+
+	private static int v(final Float value, final int default_value) { return value != null ? value.intValue() : default_value; }
 }
