2dba4cf63013db58f874331351ef27accd3569f0
Oasis <git@id.oasisfeng.com>
2017-01-16 10:52:30 +0800

    ADD: IntentFilters.equal().

--- library/src/main/java/com/oasisfeng/android/content/IntentFilters.java
+++ library/src/main/java/com/oasisfeng/android/content/IntentFilters.java
@@ -1,8 +1,12 @@
 package com.oasisfeng.android.content;
 
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.os.PatternMatcher;
+
+import java.util.Iterator;
 
 /**
  * Helpers for building {@link IntentFilter}.
@@ -38,4 +42,74 @@ public class IntentFilters {
 	public static FluentIntentFilter forActions(final String... actions) {
 		return new FluentIntentFilter().withActions(actions);
 	}
+
+	public static boolean equal(final Iterable<IntentFilter> ia, final Iterable<IntentFilter> ib) {
+		final Iterator<IntentFilter> a = ia.iterator(), b = ib.iterator();
+		while (a.hasNext()) {
+			if (! b.hasNext()) return false;
+			if (! equal(a.next(), b.next())) return false;
+		}
+		return ! b.hasNext();
+	}
+
+	public static boolean equal(final IntentFilter a, final IntentFilter b) {
+		return equal(a.actionsIterator(), b.actionsIterator())
+				&& equal(a.categoriesIterator(), b.categoriesIterator())
+				&& equal(a.typesIterator(), b.typesIterator())
+				&& equal(a.schemesIterator(), b.schemesIterator())
+				&& equal_ssp(a, b)
+				&& equal_authorities(a.authoritiesIterator(), b.authoritiesIterator())
+				&& equal_patterns(a.pathsIterator(), b.pathsIterator());
+	}
+
+	private static boolean equal(final Iterator<String> a, final Iterator<String> b) {
+		return a == b || (a != null && b != null && elementsEqual(a, b));
+	}
+
+	private static boolean equal_ssp(final IntentFilter a, final IntentFilter b) { //noinspection SimplifiableIfStatement
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) return true;
+		return equal_patterns(a.schemeSpecificPartsIterator(), b.schemeSpecificPartsIterator());
+	}
+
+	private static boolean equal_patterns(final Iterator<PatternMatcher> a, final Iterator<PatternMatcher> b) {
+		if (a == b) return true;
+		if (a == null || b == null) return false;
+		while (a.hasNext()) {
+			if (! b.hasNext()) return false;
+			final PatternMatcher o1 = a.next();
+			final PatternMatcher o2 = b.next();
+			if (! equal(o1, o2)) return false;
+		}
+		return ! b.hasNext();
+	}
+
+	private static boolean equal_authorities(final Iterator<IntentFilter.AuthorityEntry> a, final Iterator<IntentFilter.AuthorityEntry> b) {
+		if (a == b) return true;
+		if (a == null || b == null) return false;
+		while (a.hasNext()) {
+			if (! b.hasNext()) return false;
+			final IntentFilter.AuthorityEntry o1 = a.next();
+			final IntentFilter.AuthorityEntry o2 = b.next();
+			if (! equal(o1, o2)) return false;
+		}
+		return ! b.hasNext();
+	}
+
+	private static boolean equal(final PatternMatcher a, final PatternMatcher b) {
+		return a == b || (a != null && b != null && a.getType() == b.getType() && equal(a.getPath(), b.getPath()));
+	}
+
+	private static boolean equal(final IntentFilter.AuthorityEntry a, final IntentFilter.AuthorityEntry b) {
+		return a == b || (a != null && b != null && equal(a.getHost(), b.getHost()) && a.getPort() == b.getPort());
+	}
+
+	private static boolean elementsEqual(final Iterator<?> a, final Iterator<?> b) {
+		while (a.hasNext()) {
+			if (! b.hasNext()) return false;
+			if (! equal(a.next(), b.next())) return false;
+		}
+		return ! b.hasNext();
+	}
+
+	private static boolean equal(final Object a, final Object b) { return a == b || (a != null && a.equals(b)); }
 }
