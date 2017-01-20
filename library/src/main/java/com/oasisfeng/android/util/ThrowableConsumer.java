de106a728de3a77884aab8fd5fb45e00843e4d7b
Oasis <git@id.oasisfeng.com>
2017-01-20 15:49:16 +0800

    ADD: ThrowableConsumer.

---
+++ library/src/main/java/com/oasisfeng/android/util/ThrowableConsumer.java
@@ -0,0 +1,10 @@
+package com.oasisfeng.android.util;
+
+/**
+ * {@link Consumer} with {@link Throwable}
+ *
+ * Created by Oasis on 2016/11/24.
+ */
+public interface ThrowableConsumer <T, E extends Throwable> {
+	void accept(T t) throws E;
+}
