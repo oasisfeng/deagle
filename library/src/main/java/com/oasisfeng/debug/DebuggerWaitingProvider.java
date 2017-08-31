8a20ede7b53f249f816f10225295a56641846bc1
Oasis <git@id.oasisfeng.com>
2017-08-31 11:06:55 +0800

    UPDATE: DebuggerWaitingProvider skips waiting for debugger if USB is not connected.
    ADB over TCP is not supported at present.

--- library/src/main/java/com/oasisfeng/debug/DebuggerWaitingProvider.java
+++ library/src/main/java/com/oasisfeng/debug/DebuggerWaitingProvider.java
@@ -1,9 +1,12 @@
 package com.oasisfeng.debug;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
 import android.os.Debug;
 import android.util.Log;
 
+import com.oasisfeng.deagle.BuildConfig;
 import com.oasisfeng.pattern.PseudoContentProvider;
 
 /**
@@ -18,7 +21,12 @@ public class DebuggerWaitingProvider extends PseudoContentProvider {
 	private static final long WAITING_TIMEOUT = 3_000;
 
 	@Override public boolean onCreate() {
-		if ((context().getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) == 0) return false;	// Skip in non-debuggable build
+		// Skip in non-debuggable build. (BuildConfig.DEBUG may not be true since Deagle is a library dependency which is probably built as release)
+		if (BuildConfig.DEBUG || (context().getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) == 0) return false;
+		// Skip if USB is not connected. TODO: Support ADB over TCP.
+		final Intent usb_state = context().registerReceiver(null, new IntentFilter("android.hardware.usb.action.USB_STATE"));
+		if (usb_state == null || ! usb_state.getBooleanExtra("connected", false)) return false;
+
 		Log.d(TAG, "Start waiting for debugger...");
 		mWaitingThread.start();
 		try {
