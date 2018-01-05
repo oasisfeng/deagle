43c40b2603570a7b559764d4c806f0e3a3b777d7
Oasis <git@id.oasisfeng.com>
2018-01-05 22:08:11 +0800

    UPDATE: Add Suppliers.memoizeWithExpiration().

--- library/src/main/java/com/oasisfeng/android/util/Suppliers.java
+++ library/src/main/java/com/oasisfeng/android/util/Suppliers.java
@@ -13,9 +13,11 @@
  */
 package com.oasisfeng.android.util;
 
+import android.os.SystemClock;
 import android.support.annotation.VisibleForTesting;
 
 import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Useful suppliers.
@@ -100,5 +102,74 @@ public class Suppliers {
 		@Override public String toString() {
 			return "Suppliers.memoize(" + delegate + ")";
 		}
+	}
+
+	/**
+	 * Returns a supplier that caches the instance supplied by the delegate and removes the cached
+	 * value after the specified time has passed. Subsequent calls to {@code get()} return the cached
+	 * value if the expiration time has not passed. After the expiration time, a new value is
+	 * retrieved, cached, and returned. See:
+	 * <a href="http://en.wikipedia.org/wiki/Memoization">memoization</a>
+	 *
+	 * <p>The returned supplier is thread-safe. The supplier's serialized form does not contain the
+	 * cached value, which will be recalculated when {@code
+	 * get()} is called on the reserialized instance.
+	 *
+	 * @param duration the length of time after a value is created that it should stop being returned
+	 *     by subsequent {@code get()} calls
+	 * @param unit the unit that {@code duration} is expressed in
+	 * @throws IllegalArgumentException if {@code duration} is not positive
+	 * @since 2.0
+	 */
+	public static <T> Supplier<T> memoizeWithExpiration(final Supplier<T> delegate, final long duration, final TimeUnit unit) {
+		return new ExpiringMemoizingSupplier<T>(delegate, duration, unit);
+	}
+
+	@VisibleForTesting
+	static class ExpiringMemoizingSupplier<T> implements Supplier<T>, Serializable {
+		final Supplier<T> delegate;
+		final long durationMillis;
+		transient volatile T value;
+		// The special value 0 means "not yet initialized".
+		transient volatile long expirationMillis;
+
+		ExpiringMemoizingSupplier(final Supplier<T> delegate, final long duration, final TimeUnit unit) {
+			if (delegate == null) throw new NullPointerException();
+			this.delegate = delegate;
+			this.durationMillis = unit.toMillis(duration);
+			if (duration <= 0) throw new IllegalArgumentException();
+		}
+
+		@Override
+		public T get() {
+			// Another variant of Double Checked Locking.
+			//
+			// We use two volatile reads. We could reduce this to one by
+			// putting our fields into a holder class, but (at least on x86)
+			// the extra memory consumption and indirection are more
+			// expensive than the extra volatile reads.
+			long millis = expirationMillis;
+			final long now = SystemClock.uptimeMillis();
+			if (millis == 0 || now - millis >= 0) {
+				synchronized (this) {
+					if (millis == expirationMillis) { // recheck for lost race
+						final T t = delegate.get();
+						value = t;
+						millis = now + durationMillis;
+						// In the very unlikely event that millis is 0, set it to 1;
+						// no one will notice 1 ns of tardiness.
+						expirationMillis = (millis == 0) ? 1 : millis;
+						return t;
+					}
+				}
+			}
+			return value;
+		}
+
+		@Override public String toString() {
+			return "Suppliers.memoizeWithExpiration(" + delegate + ", " + durationMillis + "ms)";
+		}
+
+		private static final long serialVersionUID = 0;
 	}
 }
