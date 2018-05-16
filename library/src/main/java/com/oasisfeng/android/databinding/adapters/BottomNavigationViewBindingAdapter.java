12f4c7d79d2cff5b5348552c990571a41200e629
Oasis <git@id.oasisfeng.com>
2018-05-16 17:44:34 +0800

    ADD: BottomNavigationViewBindingAdapter with 2-way binding.

---
+++ library/src/main/java/com/oasisfeng/android/databinding/adapters/BottomNavigationViewBindingAdapter.java
@@ -0,0 +1,37 @@
+package com.oasisfeng.android.databinding.adapters;
+
+import android.databinding.BindingAdapter;
+import android.databinding.InverseBindingListener;
+import android.databinding.InverseBindingMethod;
+import android.databinding.InverseBindingMethods;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener;
+import android.view.MenuItem;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+/**
+ * Binding adapter for {@link BottomNavigationView}
+ *
+ * Created by Oasis on 2018/5/16.
+ */
+@ParametersAreNonnullByDefault @SuppressWarnings("unused") @InverseBindingMethods(
+		@InverseBindingMethod(type = BottomNavigationView.class, attribute = "selectedItemId", event = "android:selectedItemIdAttrChanged"))
+public class BottomNavigationViewBindingAdapter {
+
+	@BindingAdapter(value = {"android:onNavigationItemSelected", "android:selectedItemIdAttrChanged" }, requireAll = false)
+	public static void setOnItemSelectedListener(final BottomNavigationView view, final @Nullable OnNavigationItemSelectedListener listener,
+												 final @Nullable InverseBindingListener notifier) {
+		if (listener == null && notifier == null) view.setOnNavigationItemSelectedListener(null);
+		else view.setOnNavigationItemSelectedListener(new OnNavigationItemSelectedListener() {
+			@Override public boolean onNavigationItemSelected(final MenuItem item) {
+				if (view.getSelectedItemId() == item.getItemId()) return true;
+				if (notifier != null) view.post(new Runnable() { @Override public void run() {
+					notifier.onChange();			// Async to avoid infinite loop
+				}});
+				return listener == null || listener.onNavigationItemSelected(item);
+			}
+		});
+	}
+}
