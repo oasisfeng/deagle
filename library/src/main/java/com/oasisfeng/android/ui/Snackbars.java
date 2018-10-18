76f5598054cfcc70eb93fccd46856d072736aedc
Oasis <git@id.oasisfeng.com>
2018-10-18 13:43:03 +0800

    UPDATE: Add Snackbars.whenSwiped(Runnable callback).

--- library/src/main/java/com/oasisfeng/android/ui/Snackbars.java
+++ library/src/main/java/com/oasisfeng/android/ui/Snackbars.java
@@ -4,7 +4,6 @@ import android.content.ServiceConnection;
 import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.CheckResult;
-import android.support.customtabs.CustomTabsSession;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.TextView;
@@ -28,47 +27,35 @@ public class Snackbars {
 	}
 
 	@CheckResult public static Additional lastsForever() {
-		return new Additional() { @Override public void invoke(final Snackbar snackbar) {
-			snackbar.setDuration(Snackbar.LENGTH_INDEFINITE);
-		}};
+		return snackbar -> snackbar.setDuration(Snackbar.LENGTH_INDEFINITE);
+	}
+
+	@CheckResult public static Additional whenSwiped(final Runnable callback) {
+		return snackbar -> snackbar.addCallback(new Snackbar.Callback() { @Override public void onDismissed(final Snackbar snackbar, final int event) {
+			if (event == DISMISS_EVENT_SWIPE) callback.run();
+		}});
 	}
 
 	@CheckResult public static Additional withAction(final int label_res, final View.OnClickListener listener) {
-		return new Additional() { @Override public void invoke(final Snackbar snackbar) {
-			snackbar.setAction(label_res, listener);
-		}};
+		return snackbar -> snackbar.setAction(label_res, listener);
 	}
 
 	@CheckResult public static Additional withAction(final CharSequence label, final View.OnClickListener listener) {
-		return new Additional() { @Override public void invoke(final Snackbar snackbar) {
-			snackbar.setAction(label, listener);
-		}};
+		return snackbar -> snackbar.setAction(label, listener);
 	}
 
 	@CheckResult public static Additional withLink(final int text_res, final Uri uri) {
-		return new Additional() { @Override public void invoke(final Snackbar snackbar) {
-			final CharSequence text = snackbar.getView().getContext().getText(text_res);
-			withLink(text, uri).invoke(snackbar);
-		}};
+		return snackbar -> withLink(snackbar.getView().getContext().getText(text_res), uri).invoke(snackbar);
 	}
 
 	@CheckResult public static Additional withLink(final CharSequence text, final Uri uri) {
-		return new Additional() { @Override public void invoke(final Snackbar snackbar) {
-			snackbar.setAction(text, new View.OnClickListener() {
-				@Override public void onClick(final View v) {
-					WebContent.view(v.getContext(), uri);
-				}
-			});    // Default action before pre-cache
+		return snackbar -> {
+			snackbar.setAction(text, v -> WebContent.view(v.getContext(), uri));    // Default action before pre-cache
 			snackbar.getView().addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
 
 				@Override public void onViewAttachedToWindow(final View snackbar_view) {
-					mConnection = WebContent.preload(snackbar_view.getContext(), uri, new WebContent.OnSessionReadyListener() { @Override public void onSessionReady(final CustomTabsSession session) {
-						snackbar.setAction(text, new View.OnClickListener() {
-							@Override public void onClick(final View v) {
-								WebContent.view(v.getContext(), uri, session, null);
-							}
-						});
-					}});
+					mConnection = WebContent.preload(snackbar_view.getContext(), uri,
+							session -> snackbar.setAction(text, v -> WebContent.view(v.getContext(), uri, session, null)));
 				}
 
 				@Override public void onViewDetachedFromWindow(final View v) {
@@ -76,13 +63,12 @@ public class Snackbars {
 				}
 
 				private ServiceConnection mConnection;
-			});}
-		};
+			});};
 	}
 
 	private static Snackbar tweak(final Snackbar snackbar) {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) snackbar.getView().setZ(999);
-		final TextView msg_view = (TextView) snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
+		final TextView msg_view = snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
 		msg_view.setMaxLines(KMaxLines);	// Extend max lines
 		msg_view.setTextColor(0xffffffff);	// Workaround the light theme conflict
 		return snackbar;
