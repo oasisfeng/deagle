845b792751eb9dc0aa9ab62eb47f15a686ca5bf0
Oasis <git@id.oasisfeng.com>
2021-08-23 16:56:05 +0800

    FIX: Remove MenuAware.onShowOverflowMenu(), which is no longer working.

--- library/src/main/java/com/oasisfeng/android/databinding/adapters/SupportMenuBindingAdapter.java
+++ library/src/main/java/com/oasisfeng/android/databinding/adapters/SupportMenuBindingAdapter.java
@@ -1,15 +1,9 @@
 package com.oasisfeng.android.databinding.adapters;
 
-import android.content.Context;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 
-import com.oasisfeng.deagle.R;
-
 import androidx.annotation.MenuRes;
-import androidx.appcompat.view.menu.MenuBuilder;
-import androidx.appcompat.view.menu.MenuPresenter;
 import androidx.appcompat.widget.ActionMenuView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.BindingAdapter;
@@ -23,28 +17,11 @@ public class SupportMenuBindingAdapter {
 
 	public interface MenuAware {
 		void onBindMenu(final Menu menu);
-		void onShowOverflowMenu(final Context context, final Menu menu);
 	}
 
 	@BindingAdapter(value = {"menu", "menuAware"}, requireAll = false)
 	public static void inflateMenu(final Toolbar toolbar, final @MenuRes int last_menu_res, final MenuAware last_menu_aware,
 								   final @MenuRes int menu_res, final MenuAware menu_aware) {
-		if (menu_aware != last_menu_aware && menu_aware != null) {		// setMenuCallbacks() must be called before getMenu() to take effect.
-			toolbar.setTag(R.id.menuAware, menu_aware);
-			toolbar.setMenuCallbacks(new MenuPresenter.Callback() {
-
-				@Override public boolean onOpenSubMenu(final MenuBuilder subMenu) {
-					if (subMenu != null) return false;		// Null to indicate overflow menu.
-					final Object tag = toolbar.getTag(R.id.menuAware);
-					if ((tag instanceof MenuAware)) ((MenuAware) tag).onShowOverflowMenu(toolbar.getContext(), toolbar.getMenu());
-					else Log.e(TAG, "MenuAware tag is overridden by: " + tag);
-					return false;
-				}
-
-				@Override public void onCloseMenu(final MenuBuilder menu, final boolean allMenusAreClosing) {}
-			}, null);
-		}
-
 		if (menu_res != last_menu_res && menu_res != 0) {
 			toolbar.getMenu().clear();
 			toolbar.inflateMenu(menu_res);
@@ -59,22 +36,6 @@ public class SupportMenuBindingAdapter {
 	@BindingAdapter(value = {"menu", "menuAware"}, requireAll = false)
 	public static void inflateMenu(final ActionMenuView amv, final @MenuRes int last_menu_res, final MenuAware last_menu_aware,
 								   final @MenuRes int menu_res, final MenuAware menu_aware) {
-		if (menu_aware != last_menu_aware && menu_aware != null) {		// setMenuCallbacks() must be called before getMenu() to take effect.
-			amv.setTag(MenuAware.class.hashCode(), menu_aware);
-			amv.setMenuCallbacks(new MenuPresenter.Callback() {
-
-				@Override public boolean onOpenSubMenu(final MenuBuilder subMenu) {
-					if (subMenu != null) return false;		// Null to indicate overflow menu.
-					final Object tag = amv.getTag(R.id.menuAware);
-					if ((tag instanceof MenuAware)) ((MenuAware) tag).onShowOverflowMenu(amv.getContext(), amv.getMenu());
-					else Log.e(TAG, "MenuAware tag is overridden by: " + tag);
-					return false;
-				}
-
-				@Override public void onCloseMenu(final MenuBuilder menu, final boolean allMenusAreClosing) {}
-			}, null);
-		}
-
 		if (menu_res != last_menu_res && menu_res != 0) {
 			final Menu menu = amv.getMenu();
 			menu.clear();
@@ -86,6 +47,4 @@ public class SupportMenuBindingAdapter {
 			menu_aware.onBindMenu(menu);
 		}
 	}
-
-	private static final String TAG = "SupportMenuBinding";
 }
