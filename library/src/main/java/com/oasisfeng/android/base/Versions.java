dc75e7475534332b5e73e5eba2841458992995c2
Oasis <git@id.oasisfeng.com>
2022-01-23 00:15:53 +0800

    UPDATE: Class Versions with up-to-date Android version numbers.

--- library/src/main/java/com/oasisfeng/android/base/Versions.java
+++ library/src/main/java/com/oasisfeng/android/base/Versions.java
@@ -34,6 +34,9 @@ public class Versions {
     /** @param v version code as of {@link android.os.Build.VERSION#SDK_INT} */
     public static String getAndroidVersionNumber(final int v) {
         switch (v) {
+        case 32: return "12L";
+        case 31: return "12";
+        case 30: return "11";
         case 29: return "10";
         case 28: return "9";
         case 27: return "8.1";
@@ -43,7 +46,7 @@ public class Versions {
         case 23: return "6";
         case 22: return "5.1";
         case 21: return "5.0";
-        case 20: return "4.4W";
+        case 20:
         case 19: return "4.4";
         case 18: return "4.3";
         case 17: return "4.2";
@@ -54,7 +57,7 @@ public class Versions {
         case 12: return "3.1";
         case 11: return "3.0";
         default:
-            if (v > 28) return "9+";
+            if (v > 32) return String.valueOf(v - 20);     // Android 13+
             if (v > 4) return "2.x";
             return "1.x";
         }
