d3d74418c069c77a8bc5619329e138bad99b9ad4
Oasis <git@id.oasisfeng.com>
2024-01-03 21:08:11 +0800

    UPDATE: Add special STATE_COLLAPSED_OR_EXPANDED to BottomSheetBindingAdapter.

--- library/src/main/java/com/oasisfeng/android/databinding/adapters/BottomSheetBindingAdapter.java
+++ library/src/main/java/com/oasisfeng/android/databinding/adapters/BottomSheetBindingAdapter.java
@@ -16,12 +16,19 @@ import androidx.databinding.BindingAdapter;
 @SuppressWarnings("unused")
 public class BottomSheetBindingAdapter {
 
+	/** Keep collapsed or expanded state as is, or switch to collapsed state. */
+	public static final int STATE_COLLAPSED_OR_EXPANDED = 99;
+
 	@BindingAdapter("behavior_state")
 	public static void setBottomSheetState(final View view, final @BottomSheetBehavior.State int state) {
 		final BottomSheetBehavior<View> behavior = BottomSheetBehavior.from(view);
 		if (behavior == null) return;
 		try {
-			behavior.setState(state);
+			if (state == STATE_COLLAPSED_OR_EXPANDED) {
+				final int lastState = behavior.getState();
+				if (lastState != BottomSheetBehavior.STATE_COLLAPSED && lastState != BottomSheetBehavior.STATE_EXPANDED)
+					behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+			} else behavior.setState(state);
 		} catch (final RuntimeException e) {
 			new Handler().post(new Runnable() { @Override public void run() {
 				try {
