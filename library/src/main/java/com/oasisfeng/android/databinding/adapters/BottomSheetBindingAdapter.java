95f3a00028feaca8ffa6d9ed062694190c859459
Oasis <git@id.oasisfeng.com>
2024-01-03 23:56:16 +0800

    UPDATE: Minor tweaks.

--- library/src/main/java/com/oasisfeng/android/databinding/adapters/BottomSheetBindingAdapter.java
+++ library/src/main/java/com/oasisfeng/android/databinding/adapters/BottomSheetBindingAdapter.java
@@ -3,10 +3,10 @@ package com.oasisfeng.android.databinding.adapters;
 import android.os.Handler;
 import android.view.View;
 
+import androidx.databinding.BindingAdapter;
+
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback;
-
-import androidx.databinding.BindingAdapter;
 
 /**
  * Binding adapter for {@link BottomSheetBehavior}
@@ -22,7 +22,6 @@ public class BottomSheetBindingAdapter {
 	@BindingAdapter("behavior_state")
 	public static void setBottomSheetState(final View view, final @BottomSheetBehavior.State int state) {
 		final BottomSheetBehavior<View> behavior = BottomSheetBehavior.from(view);
-		if (behavior == null) return;
 		try {
 			if (state == STATE_COLLAPSED_OR_EXPANDED) {
 				final int lastState = behavior.getState();
@@ -30,18 +29,15 @@ public class BottomSheetBindingAdapter {
 					behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
 			} else behavior.setState(state);
 		} catch (final RuntimeException e) {
-			new Handler().post(new Runnable() { @Override public void run() {
-				try {
-					behavior.setState(state);
-				} catch (final RuntimeException ignored) {}
-			}});
+			new Handler().post(() -> {
+				try { behavior.setState(state); } catch (final RuntimeException ignored) {}
+			});
 		}
 	}
 
 	@BindingAdapter("behavior_bottomSheetCallback")
 	public static void bindBottomSheetCallback(final View view, final BottomSheetCallback callback) {
 		final BottomSheetBehavior<View> behavior = BottomSheetBehavior.from(view);
-		if (behavior == null) throw new IllegalArgumentException(view + " has no BottomSheetBehavior");
 		behavior.setBottomSheetCallback(callback);
 	}
 }
